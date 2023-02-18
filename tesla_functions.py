24c04cffc1bcc4da200ae0a6a5edc82b122a04d6
rinkeee <63800716+rinkeee@users.noreply.github.com>
2023-02-18 17:18:05 +0100

    Update tesla_functions.py

--- tesla_functions.py
+++ tesla_functions.py
@@ -1,5 +1,5 @@
 import pandas as pd 
-import plotly.express as px
+# import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from datetime import datetime
